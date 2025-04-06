96cd206c0a468da8f4564cd061f94ad8810fa7f2
DAMARSL <165367916+DAMARSL@users.noreply.github.com>
2025-04-06 20:43:17 +0300

    Update untitled21.py

--- untitled21.py
+++ untitled21.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1Q2j8yMH4nEGDNenGCOFthZjjClPZhJvZ
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
