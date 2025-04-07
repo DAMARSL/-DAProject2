6fe2adbeb5d66039b5e4b65c8ff53ab7d783f5fa
DAMARSL <165367916+DAMARSL@users.noreply.github.com>
2025-04-07 16:20:31 +0300

    Update untitled21.py

--- untitled21.py
+++ untitled21.py
@@ -16,7 +16,8 @@ import pickle
 st.markdown("""
     <style>
     body {
-        background-image: url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fblue-abstract-background&psig=AOvVaw0T_qBjaGpRei4bgUw26v4w&ust=1744117972076000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjTuvr_xYwDFQAAAAAdAAAAABAJ');
+        background-image: url(''https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fblue-abstract-background&psig=AOvVaw0T_qBjaGpRei4bgUw26v4w&ust=1744117972076000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMjTuvr_xYwDFQAAAAAdAAAAABAJ'
+');
         background-size: cover;
         background-position: center;
     }
